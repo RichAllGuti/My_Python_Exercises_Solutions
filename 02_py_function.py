c7bcf5d0b90fc4965b1d666e0ba387447c58347e
RichAllGuti <richjohnall@gmail.com>
2023-03-19 14:03:23 -0600

    change the name of function one and add the function 2 which consist in the calculation area of a interesting polygon by a given n number

---
+++ 02_py_function.py
@@ -0,0 +1,14 @@
+"""
+A n-insteresting polygon is just a square with a side of length.
+Find the area of a polygon for a given 'n'.
+"""
+#
+def calculate_polygon_area(n):
+    #
+    # validate if n is defferent of number 1, return 1 if it True
+    if n == 1:
+        return 1
+    else:   # return the result of n-1 exponentiation to 2 and n exponentiation to 2 
+        return (n-1)**2 + n**2
+#
+print(calculate_polygon_area(4))
