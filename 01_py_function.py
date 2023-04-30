c88a0a4abe12a737444d6f9b5b9498cdb034733f
RichAllGuti <richjohnall@gmail.com>
2023-04-29 23:49:21 -0600

    Added a docstring to explain what the function does. Removed the unnecessary f-string formatting in the return statements. Assigned the reversed string to a variable for better readability and code organization. Simplified the return statement to return the boolean value directly instead of a string representation of True or False

--- 01_py_function.py
+++ 01_py_function.py
@@ -2,11 +2,17 @@
 # With a given string please verify if it is palindrome.
 #
 def is_palindrome(p_string):
-    #
-    #Validating whether the p_string is a palindrome using python slice notation in the [::-1] to reverse the string for compares it with the p_string input.
-    if p_string == p_string[::-1]:
-        return f"{True} Is a palindrom."
+    """
+    This function takes a string and returns True if it is a palindrome, False otherwise.
+    """
+    # Using python slice notation in the [::-1] to reverse the string for compares it with the p_string input.
+    reversed_string = p_string[::-1]
+    
+    # Checking if the reversed string is equal to the original string
+    if p_string == reversed_string:
+        return True
     else:
-        return f"{False} Is not a palindrom"
+        return False
 
+# Example usage
 print(is_palindrome('oso'))
