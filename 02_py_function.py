f9c7749c880e44f439041f5b37a1da3839e0ca12
RichAllGuti <richjohnall@gmail.com>
2023-04-30 00:22:27 -0600

    Added a docstring to explain what the function does and  made sure the code conforms to PEP 8 style guidelines.

--- 02_py_function.py
+++ 02_py_function.py
@@ -1,14 +1,17 @@
 """
-A n-interesting polygon is just a square with a side of length.
 Find the area of a polygon for a given 'n'.
 """
 #
 def calculate_polygon_area(n):
-    #
-    # validate if n is defferent of number 1, return 1 if it True
+    """
+    This function takes an integer 'n' and returns the area of an n-interesting polygon, where an n-interesting polygon
+    is defined as a square with n layers of cells on each side.
+    """
     if n == 1:
         return 1
-    else:   # return the result of n-1 exponentiation to 2 and n exponentiation to 2 
+    else:
+        # Calculate the area by summing the area of the previous n-1 square layers and adding the area of the nth layer.
         return (n-1)**2 + n**2
-#
+
+# Example usage
 print(calculate_polygon_area(4))
